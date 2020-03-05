02ada8a603e88bf099eae211c659d97b0ba36d57
David Jönell <david_jonell@hotmail.com>
2020-03-05 11:37:28 +0100

    adding registration

---
+++ src/Vaderstad.Web/client/components/mediaprovider/registration/registration.manifest.ts
@@ -0,0 +1,12 @@
+﻿import { Composer } from "@omnia/tooling-composers";
+import { OmniaService, OmniaWebComponentManifests } from '@omnia/fx-models';
+
+Composer.registerManifest("aa5dea70-4521-43d0-ae08-ce15e13e262e", "vab.mediapicker")
+    .registerResources({
+        resourcePaths: ["./registration.js"]
+    })
+    .withLoadRules()
+    .loadIfManifestLoaded({
+        omniaServiceId: OmniaService.Id.toString(),
+        resourceId: OmniaWebComponentManifests.FxUxMediapicker.toString()
+    })
---
+++ src/Vaderstad.Web/client/components/mediaprovider/registration/registration.ts
@@ -0,0 +1,15 @@
+﻿import { Topics } from "@omnia/fx";
+import { MediaPickerProviderMediaTypes } from '@omnia/fx/ux';
+
+
+Topics.registerMediaPickerProvider.publish(
+    {
+        category: "image",
+        name: "VAB Images",
+        iconClass: "",     
+        elementNameToRender: "vab-media-picker-extension",
+        mediaType: MediaPickerProviderMediaTypes.image,
+        hasTextSearchSupport: true,
+        sortOrder: 50
+    }
+)
